bbd9b8f5423b93466792394cc9c46d3fadd4bad0
TerentevVlad <ter.vlad.ter@gmail.com>
2023-03-05 20:55:03 +0300

    Добавил создание enumerable для листа

--- SaberTest/LinkedListRand/ListRand.cs
+++ SaberTest/LinkedListRand/ListRand.cs
@@ -7,11 +7,42 @@ namespace LinkedListRand
         public ListNode Tail;
         public int Count;
         
+        
         public void Serialize(FileStream s)
         {
-            Console.WriteLine("Open: " + s.Name);
+            var enumerable = this.CreateEnumerable();
+            foreach (var node in enumerable)
+            {
+                Console.WriteLine(node.Data);
+            }
         }
         public void Deserialize(FileStream s)
+        {
+           
+        }
+    }
+
+    public static class ListRandExtension
+    {
+        public static IEnumerable<ListNode> CreateEnumerable(this ListRand list)
+        {
+            List<ListNode> listNodes = new List<ListNode>();
+            
+            ListNode currentNode = list.Head;
+            while (currentNode != null)
+            {
+                listNodes.Add(currentNode);
+                currentNode = currentNode.Next;
+            }
+
+            return listNodes;
+        }
+    }
+
+
+    public static class BinaryWriterExtension
+    {
+        public static void WriteNode(this BinaryWriter writer)
         {
             
         }
