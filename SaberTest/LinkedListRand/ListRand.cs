d254e86168eb454af79879c44e186fe7fe4d89ba
TerentevVlad <ter.vlad.ter@gmail.com>
2023-03-05 20:22:57 +0300

    Добавил открытие и создание файла. Инициализация листа

---
+++ SaberTest/LinkedListRand/ListRand.cs
@@ -0,0 +1,19 @@
+﻿
+namespace LinkedListRand
+{
+    public class ListRand
+    {
+        public ListNode Head;
+        public ListNode Tail;
+        public int Count;
+        
+        public void Serialize(FileStream s)
+        {
+            Console.WriteLine("Open: " + s.Name);
+        }
+        public void Deserialize(FileStream s)
+        {
+            
+        }
+    }
+}
---
+++ SaberTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using LinkedListRand;
+
+
+var node0 = new ListNode() { Data = "1_ tst: + {g} - " };
+var node1 = new ListNode() { Data = "2_tst: /n {g}" };
+var node2 = new ListNode() { Data = "3_tst: + {g/t} ] / " };
+
+node0.Next = node1;
+node1.Next = node2;
+node2.Next = null;
+
+node0.Prev = null;
+node1.Prev = node0;
+node2.Prev = node1;
+
+
+node0.Rand = null;
+node1.Rand = node0;
+node2.Rand = node2;
+
+var list = new ListRand()
+{
+    Head = node0,
+    Tail = node2,
+    Count = 5
+};
+
+
+list.Serialize(File.Open("SerializedList.txt", FileMode.OpenOrCreate));
+
